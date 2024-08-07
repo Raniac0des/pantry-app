5a01b6a3674cc498eb1b41a2c9b453e7dcd2f252
raniam101 <Rmaji3@uic.edu>
2024-08-07 13:06:55 -0500

    fixing page.js 'amount' from 'price'

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
+'use client'
 import React, {useState, useEffect} from 'react'; 
 import Image from "next/image";
 
 export default function Home() {
+  const[ items, setItems] = useState ([ 
+    {name: 'Vegetables', amount: 5.00},
+    { name: 'Fruits', amount: 5.00 },
+    {name: 'Carbohydrates', amount: 4.00},
+    {name: 'candy', amount: 10.00},
+
+    const [total, setTotal] = useState(0)
+
+    ]); 
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -25,6 +37,15 @@ export default function Home() {
               Add
             </button>
           </form>
+            {items.map((item, id)=> (
+            <li key={id} className= 'my-4w full flex justify-between '> 
+              <div className= 'p-4 w-full flex justify-between'> 
+                <span className= 'capitalize'>{item.name}</span>
+                <span>${item.price}</span>
+              </div>
+              <button className = 'ml-8 p-4 border-l-2 border-slate-900 hover: bg-slate-900 w-16>X</button>
+            </li>
+            ))} 
         </div>
       </div>
     </main>
