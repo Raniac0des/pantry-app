e960370d1d766a486f9917cc13e99ac1cd20aba9
raniam101 <Rmaji3@uic.edu>
2024-08-07 14:41:59 -0500

    deleted an error

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 'use client'
 import React, {useState, useEffect} from 'react'; 
 import Image from "next/image";
+import { collection, addDoc } from "firebase/firestore"; 
+
 
 export default function Home() {
   const [items, setItems] = useState([ 
@@ -9,26 +11,48 @@ export default function Home() {
     {name: 'Carbohydrates', price: 4.00},
     {name: 'Candy', price: 10.00},
   ]);
+  const [newItem, setNewItem] = useState({name: '', price: ''})
   const [total, setTotal] = useState(0)
+
+  // add item to database 
+  const addItem = async (e) => { 
+    e.preventDefault()
+    if(newItem.name !== '' && newItem.price !== '') {
+      //setItems([...items, newItem])
+      await addDoc(collection(db, 'items'), {
+        name: newItem.name.trim(), 
+        price: newItem.price, 
+    }); 
+    }
+  }
+
+  // read items from database 
+
+  //delete items from database 
 
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl p-4 text-center">Pantry Tracker</h1>
+        <h1 className="text-4xl p-4 text-center">Pantry Expense Tracker</h1>
         <div className="bg-slate-800 p-5 rounded-lg">
           <form className="grid grid-cols-6 items-center text-black">
             <input
+            value= {newItem.name}
+            onChange={(e) => setNewItem({...newItem, name: e.target.value})}
               className="col-span-3 p-3 border"
               type="text"
               placeholder="Enter Item:"
             />
             <input
+            value= {newItem.price}
+            onChange={(e) => setNewItem({...newItem, price: e.target.value})}
               className="col-span-2 p-3 border mx-3"
               type="number"
-              placeholder="Enter quantity:"
+              placeholder="Enter price:"
             />
             <button
+            onClick= {addItem}
               className="text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl col-span-1"
               type="submit"
             >
@@ -36,7 +60,7 @@ export default function Home() {
             </button>
           </form>
           <ul> 
-            {items.map((item, id)=> (
+            {items.map} 
             <li key={id} className= 'my-4w full flex justify-between '> 
               <div className= 'p-4 w-full flex justify-between'> 
                 <span className= 'capitalize'>{item.name}</span>
@@ -46,7 +70,6 @@ export default function Home() {
                 X
               </button>
             </li>
-            ))} 
             </ul> 
             {items.length < 1 ? (''): ( 
               <div className= 'flex justify-between p-3'>
